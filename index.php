61fe1fef33ec1b7b122b3ccf8c0fe7a1c719c3b2
adeyura <ade.yusuf.r@gmail.com>
2017-03-29 12:43:42 +0700

    add index.php + jadwal.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="jadwal.php" method="post" enctype="multipart/form-data">
+    Data pengajar:
+    <input type="file" name="dataPengajar" id="fileToUpload">
+	<br>
+    Data kelas kursus:
+    <input type="file" name="dataKelasKursus" id="fileToUpload">
+	<br>
+    Data pengajar:
+    <input type="file" name="dataPengajar" id="fileToUpload">
+	<br>
+    <input type="submit" value="Jadwalkan" name="submit">	
+</form>
+
+</body>
+</html>
