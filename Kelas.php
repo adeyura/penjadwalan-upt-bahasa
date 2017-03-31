694fb2af4c3c104ec9d4335603a2b4f0b4038da6
steffiindrayani <steffiindrayani16@gmail.com>
2017-03-31 23:29:17 +0700

    Update Kelas.php

--- Kelas.php
+++ Kelas.php
@@ -4,10 +4,22 @@
 		public static $lastKelasId;
 		public $kelasId;
 		public $name;
+		public $defaultstart;
+		public $defaultend;
+		public $duration;
+		public $defaultdays = array(1,1,1,1,1,1);
 
 		/* Member functions */
-		public function __construct($nm) {
+		public function __construct($nm, $s, $e, $dur, $d1, $d2, $d3, $d4, $d5) {
 			$this->name = $nm;
+			$this->defaultstart = $s;
+			$this->defaultend = $e;
+			$this->duration = $dur;
+			$this->defaultdays[1] = $d1;
+			$this->defaultdays[2] = $d2;
+			$this->defaultdays[3] = $d3;
+			$this->defaultdays[4] = $d4;
+			$this->defaultdays[5] = $d5;
 			$this->kelasId = Kelas::$lastKelasId++;
 		}
 
@@ -18,6 +30,29 @@
 		
 		function getName() {
 			return $this->name;
+		}
+		
+		function getStartTime(){
+			return $this->defaultstart;
+		}
+		function getEndTime(){
+			return $this->defaultend;
+		}
+		function getDuration(){
+			return $this->duration;
+		}
+		
+		//predikat
+		function isDayDefault($i) {
+			return $this->defaultdays[$i];
+		}
+		
+		function isTimeDefault($b, $e) {
+			return ($this->defaultstart <= $b && $b <= $this->defaultend && $this->defaultstart <= $e && $e <= $this->defaultend);
+		}
+		
+		function isDefault($day, $b, $e) {
+			return isDayDefault($day) && isTimeDefault($b, $e);
 		}
 	}
 	
